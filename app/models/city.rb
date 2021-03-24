304e27eafbc06f86f6bcaf05a4ec13f7b6145fbe
figo5530 <figooooo0221@gmail.com>
2021-03-23 21:21:14 -0400

    commit tree 79702ee9de5a8022ad4163e7dfba083b43d8d662

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,10 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+
+  def city_openings(starting_date, ending_date)
+    Listing.joins(:reservations).where.not(reservations: {checkin: starting_date..ending_date})
+    Listing.joins(:reservations).where.not(reservations: {checkout: starting_date..ending_date})
+  end
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, presence: true
+  validates :checkout, presence: true
 
+  def duration
+    self.checkout - self.checkin
+  end
+
+  def total_price
+    self.duration * self.listing.price
+  end
 end
